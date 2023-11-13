208a87a18f2b7cac81353d7f953b9c40cd75dfb4
Shunri Zheng <sz3091@columbia.edu>
2023-11-13 08:36:47 -0500

    Successful Run

---
+++ plot_data.py
@@ -0,0 +1,16 @@
+import os
+import matplotlib.pyplot as plt
+
+f = open("Average_Reward.txt","r")
+X_Dom = []
+Y_Dom = []
+
+for line in f.readlines():
+    A = line.split("   ")
+    X_Dom.append(int(A[0]))
+    Y_Dom.append((float(A[1].split("\n")[0])))
+
+print(len(X_Dom))
+print(len(Y_Dom))
+plt.plot(X_Dom,Y_Dom)
+plt.savefig('Graph.png')
