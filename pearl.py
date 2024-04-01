045b4e1fe5508b0acbbc463a033256d9830a7314
Satwik290896 <vsaisatwik1996@gmail.com>
2024-04-01 00:23:03 -0400

    Added MetaLLMPolicy() with Softmax as final_Activation

--- pearl.py
+++ pearl.py
@@ -247,8 +247,10 @@ def torch_ify(np_array_or_other):
 
 def np_ify(tensor_or_other):
     if isinstance(tensor_or_other, Variable):
+        print(f"np_ify(): is_instance? converting to numpy")
         return ptu.get_numpy(tensor_or_other)
     else:
+        print(f"np_ify(): is_instance? Leaving it as it is")
         return tensor_or_other
 
 def create_stats_ordered_dict(
@@ -411,6 +413,7 @@ class FlattenMlp(Mlp):
     """
 
     def forward(self, *inputs, **kwargs):
+        #print(f"FlattenMlp(): forward(): inputs = {inputs}")
         flat_inputs = torch.cat(inputs, dim=1)
         return super().forward(flat_inputs, **kwargs)
 
@@ -518,6 +521,105 @@ class RecurrentEncoder(FlattenMlp):
 
 
 
+
+
+class MetaLLMPolicy(Mlp, ExplorationPolicy):
+    """
+    Usage:
+
+    ```
+    policy = TanhGaussianPolicy(...)
+    action, mean, log_std, _ = policy(obs)
+    action, mean, log_std, _ = policy(obs, deterministic=True)
+    action, mean, log_std, log_prob = policy(obs, return_log_prob=True)
+    ```
+    Here, mean and log_std are the mean and log_std of the Gaussian that is
+    sampled from.
+
+    If deterministic is True, action = tanh(mean).
+    If return_log_prob is False (default), log_prob = None
+        This is done because computing the log_prob can be a bit expensive.
+    """
+
+    def __init__(
+            self,
+            hidden_sizes,
+            obs_dim,
+            latent_dim,
+            action_dim,
+            std=None,
+            init_w=1e-3,
+            **kwargs
+    ):
+        self.save_init_params(locals())
+        super().__init__(
+            hidden_sizes,
+            input_size=obs_dim,
+            output_size=26,
+            init_w=init_w,
+            **kwargs
+        )
+        self.latent_dim = latent_dim
+        self.log_std = None
+        self.std = std
+        if std is None:
+            last_hidden_size = obs_dim
+            if len(hidden_sizes) > 0:
+                last_hidden_size = hidden_sizes[-1]
+            self.last_fc_log_std = nn.Linear(last_hidden_size, action_dim)
+            self.last_fc_log_std.weight.data.uniform_(-init_w, init_w)
+            self.last_fc_log_std.bias.data.uniform_(-init_w, init_w)
+        else:
+            self.log_std = np.log(std)
+            assert LOG_SIG_MIN <= self.log_std <= LOG_SIG_MAX
+
+    def get_action(self, obs, deterministic=False):
+        actions = self.get_actions(obs, deterministic=deterministic)
+        print(f"MetaLLMPolicy(): get_action: {actions}, {type(actions)}")
+        return actions, {}
+
+    @torch.no_grad()
+    def get_actions(self, obs, deterministic=False):
+        outputs = self.forward(obs, deterministic=deterministic)[0]
+        return np_ify(outputs)
+
+    def forward(
+            self,
+            obs,
+            reparameterize=False,
+            deterministic=False,
+            return_log_prob=False,
+    ):
+        """
+        :param obs: Observation
+        :param deterministic: If True, do not sample
+        :param return_log_prob: If True, return a sample and its log probability
+        """
+        h = obs
+        for i, fc in enumerate(self.fcs):
+            h = self.hidden_activation(fc(h))
+        mean = self.last_fc(h)
+        if self.std is None:
+            log_std = self.last_fc_log_std(h)
+            log_std = torch.clamp(log_std, LOG_SIG_MIN, LOG_SIG_MAX)
+            std = torch.exp(log_std)
+        else:
+            std = self.std
+            log_std = self.log_std
+
+        log_prob = None
+        expected_log_prob = None
+        mean_action_log_prob = None
+        pre_tanh_value = None
+        print(f"MetaLLMPolicy(): mean: {mean}, size: {mean.size()}")
+        #action = (torch.argmax(torch.nn.functional.log_softmax(mean)) + 1).view([1,1])
+        action = (torch.argmax(torch.nn.functional.log_softmax(mean)))
+        print(f"MetaLLMPolicy(): pre-action: {(torch.nn.functional.log_softmax(mean)).size()}, pre-action(tanh): {(torch.tanh(mean)).size()}, action: {action}")
+        return (
+            action, mean, log_std, log_prob, expected_log_prob, std,
+            mean_action_log_prob, pre_tanh_value,
+        )
+    
 
 
 
@@ -692,7 +794,7 @@ def elem_or_tuple_to_variable(elem_or_tuple):
 
 def filter_batch(np_batch):
     for k, v in np_batch.items():
-        if v.dtype == np.bool:
+        if v.dtype == bool:
             yield k, v.astype(int)
         else:
             yield k, v
@@ -1660,9 +1762,11 @@ class PEARLSoftActorCritic(MetaRLAlgorithm):
         # data is (task, batch, feat)
         obs, actions, rewards, next_obs, terms = self.sample_sac(indices)
 
+        print(f"PEARLSoftActorCritic(): _take_step(): agent = {self.agent}")
         # run inference in networks
         policy_outputs, task_z = self.agent(obs, context)
         new_actions, policy_mean, policy_log_std, log_pi = policy_outputs[:4]
+        print(f"PEARLSoftActorCritic(): _take_step():new_actions = {new_actions}")
 
         # flattens out the task dimension
         t, b, _ = obs.size()
@@ -1804,7 +1908,7 @@ def experiment(variant):
     tasks = env.get_all_task_idx()
     print("All Tasks: ", tasks)
     obs_dim = int(np.prod(env.observation_space.shape))  #20 for cheetah-vel
-    action_dim = int(np.prod(env.action_space.shape))    #6 for cheetah-vel
+    action_dim = 26    #6 for cheetah-vel
     reward_dim = 1
 
     # instantiate networks
@@ -1835,7 +1939,7 @@ def experiment(variant):
         input_size=obs_dim + latent_dim,
         output_size=1,
     )
-    policy = TanhGaussianPolicy(
+    policy = MetaLLMPolicy(
         hidden_sizes=[net_size, net_size, net_size],
         obs_dim=obs_dim + latent_dim,
         latent_dim=latent_dim,
